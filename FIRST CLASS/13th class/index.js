75c7ff0472aeb00d03e90a18679d26c4daababaf
Abdullah <abdullakq6@gmail.com>
2025-12-31 00:44:12 +0500

    updated code

---
+++ FIRST CLASS/13th class/index.js
@@ -0,0 +1,17 @@
+// var day ="mon";
+
+var day = prompt("enter a day")
+
+switch(day){
+case "mon" :
+console.log("today is monday");
+break
+case "tues" :
+console.log("today is tuesday");
+break
+case "wen" :
+console.log("today is wenesday");
+break
+default :
+console.log("enter a write number");
+}
