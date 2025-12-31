201839ea37cedf4b9b1eb25ec87c47d85c0c87b3
Abdullah <abdullakq6@gmail.com>
2025-12-31 22:08:25 +0500

    updated code

--- FIRST CLASS/13th class/index.js
+++ FIRST CLASS/13th class/index.js
@@ -1,17 +1,52 @@
+// SWITCH STATEMENT :
 // var day ="mon";
 
-var day = prompt("enter a day")
+// var day = prompt("enter a day")
 
-switch(day){
-case "mon" :
-console.log("today is monday");
-break
-case "tues" :
-console.log("today is tuesday");
-break
-case "wen" :
-console.log("today is wenesday");
-break
-default :
-console.log("enter a write number");
+// switch(day){
+// case "mon" :
+// console.log("today is monday");
+// break
+// case "tues" :
+// console.log("today is tuesday");
+// break
+// case "wen" :
+// console.log("today is wenesday");
+// break
+// default :
+// console.log("enter a write number");
+// }
+// IMPORTANT LINES :
+// if else
+// if condition complex
+// slow for single condition
+// more flexible for complex
+
+// switch
+// if single condition 
+// slow for complex condition
+
+// main difference between switch and if-else is (default)
+// else execute when if-else
+// default execute when we need to show false condition
+// // IMPORTANT LINES END:
+// // SWITCH STATEMENT END:
+
+// -----WHILE LOOP---- :
+var i = 0
+while (i < 5) {
+    console.log(i)
+    i++
 }
+
+// -----WHILE LOOP END---- :
+// do-while-loop:
+var i = 0
+do{
+      console.log(i)
+    i++
+} while (i < 2)
+// do-while-loop END:
+
+
+
