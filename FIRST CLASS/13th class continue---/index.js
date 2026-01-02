025047e978e256a229fb03eb0f249aeb1b3c47eb
Abdullah <abdullakq6@gmail.com>
2026-01-02 14:42:23 +0500

    updated code

---
+++ FIRST CLASS/13th class continue---/index.js
@@ -0,0 +1,10 @@
+//  function showPara(){
+// var p = document.getElementById("para").innerText ="Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, consequuntur! Mollitia atque quasi dolore id inventore asperiores iste excepturi debitis repellendus accusamus aut sint tenetur optio blanditiis libero enim, esse voluptate? Expedita repudiandae assumenda, dolores, voluptatem alias ut quasi quidem dolor blanditiis labore ad impedit! Accusamus, ad praesentium! Doloribus praesentium eaque, natus esse culpa ad quam quos obcaecati iure voluptate sed quibusdam explicabo illo dolorem libero tenetur ipsa sequi temporibus nihil! Perspiciatis, rem. Rerum vitae quidem, saepe neque aperiam ut ullam sequi recusandae! Facilis, ratione harum. Dolore iure optio nulla dignissimos, inventore vitae vero et cumque id tenetur facere aut."
+//  }
+ var p = document.getElementById('inputField').style.color="white"
+ console.log("p->",p)
+ 
+  function redfield(event){
+        console.log("event=>",event.style.width = "900px")
+        event.style.width = "900px"
+      }
