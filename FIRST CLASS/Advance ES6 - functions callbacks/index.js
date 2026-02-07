0eaac125f97e8f522b1cdc38dae20fbd70da57ae
Abdullah <abdullakq6@gmail.com>
2026-02-07 07:49:49 +0500

    update code

--- FIRST CLASS/Advance ES6 - functions callbacks/index.js
+++ FIRST CLASS/Advance ES6 - functions callbacks/index.js
@@ -3,3 +3,18 @@
 // const obj3 = Object.create(null)
 // obj3.userName="ali"
 
+// var address = prompt("enter address");
+// var country = prompt("enter country country");
+// const obj3 = {
+//   bio: ["ali", "khan", 20],
+//   address: { city: "karachi", country: "pakistan", state: "sindh" },
+// };
+// console.log(obj3[data][country])
+// console.log(obj3.address.country);
+// console.log(obj3.bio["1"])
+
+// var firstKey = prompt("enter first key");
+// var secondKey = prompt("enter second key");
+// const obj3 = {
+//   bio: ["ali", "khan", 20],
+//   address: {
