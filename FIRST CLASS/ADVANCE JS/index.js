31f5fe7f472fe839811cca13f3c2cf2962d4b615
Abdullah <abdullakq6@gmail.com>
2026-02-07 07:47:00 +0500

    update code

--- FIRST CLASS/ADVANCE JS/index.js
+++ FIRST CLASS/ADVANCE JS/index.js
@@ -3,13 +3,3 @@ var stData = {
     snroll : 10,
 
 
-    //  detail:function() {
-    //     return `name is ${this.sname} and roll no is ${this.snroll}`//es5
-
-    // }
-
-    detail() {
-        return  `name is ${this.sname} and roll no is ${this.snroll}`//es6
-    }
-}
-console.log(stData.detail())
