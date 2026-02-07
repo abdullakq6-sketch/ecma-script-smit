ddb80024c2ef15c4423ea523699921ec3569312f
Abdullah <abdullakq6@gmail.com>
2026-02-07 08:00:48 +0500

    update code

--- FIRST CLASS/advance - IIFE - recursion closure/index.js
+++ FIRST CLASS/advance - IIFE - recursion closure/index.js
@@ -18,3 +18,23 @@ console.log("Hello world");
 
 // (function(myname,cast){
 //     console.log(myname,cast)
+// })('ali','awan');
+
+// let result = (function(uname,uage){
+//   return uname + uage
+// // console.log(uname,uage)
+// })('ali',30)
+
+// let result = (uname,uage)=> uname + uage
+
+// console.log(result)
+
+// function add(a,b){
+// return a + b
+// }
+// const add=(a,b)=>{return a + b}
+// console.log(add(2,3))
+
+// var uname = "khan"
+// const obj ={
+//     uname:"ali",
